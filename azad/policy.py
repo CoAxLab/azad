6620af6c450e9e926006b2f52624f0a70f596735
Erik Peterson <Erik.Exists@gmail.com>
2018-05-10 11:17:36 -0700

    Normalized type (again) for ep_greedy

--- azad/policy.py
+++ azad/policy.py
@@ -5,6 +5,9 @@ def ep_greedy(x, ep):
     if torch.rand(1) > ep:
         action = torch.argmax(x).unsqueeze(0)
     else:
-        action = torch.randint(0, x.shape[0], (1, ), dtype=torch.int)
+        action = torch.randint(0, x.shape[0], (1, ))
+
+    # Ensure type consistency... 
+    action = action.type(torch.FloatTensor)
 
     return action
