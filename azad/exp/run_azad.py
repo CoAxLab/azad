4d972b8229afeddc0e9107452031db724cd382e1
Erik Peterson <Erik.Exists@gmail.com>
2018-05-10 21:10:54 -0700

    Clean imports

--- azad/exp/run_azad.py
+++ azad/exp/run_azad.py
@@ -1,38 +1,8 @@
 #!/usr/bin/env python3
 """Run azad experiments"""
 import fire
-import torch
 
-from math import exp
-
-import numpy as np
-
-import gym
-from gym import wrappers
-
-import torch.optim as optim
-import torch.nn.functional as F
-from torch.autograd import Variable
-
-from azad.stumblers import TwoQN
-from azad.stumblers import ThreeQN
-from azad.stumblers import DQN
-from azad.policy import epsilon_greedy
-from azad.util import ReplayMemory
-from azad.util import plot_cart_durations
 from azad.exp import exp_1
-
-import matplotlib.pyplot as plt
-
-# ---------------------------------------------------------------
-# Handle dtypes for the device
-USE_CUDA = torch.cuda.is_available()
-FloatTensor = torch.cuda.FloatTensor if USE_CUDA else torch.FloatTensor
-LongTensor = torch.cuda.LongTensor if USE_CUDA else torch.LongTensor
-ByteTensor = torch.cuda.ByteTensor if USE_CUDA else torch.ByteTensor
-Tensor = FloatTensor
-
-# ---------------------------------------------------------------
 
 
 def exp_build():
