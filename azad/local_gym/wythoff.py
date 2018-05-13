3da912c23461086b4dbbca6215b016b8d1ffebbd
Erik Peterson <Erik.Exists@gmail.com>
2018-05-12 22:31:35 -0700

    COmplete wyoff game.

--- azad/local_gym/wythoff.py
+++ azad/local_gym/wythoff.py
@@ -4,6 +4,15 @@ from gym import spaces
 from gym.utils import seeding
 
 
+def optimal_move(state, m, n):
+    # every step is followed by an optimal opponent play
+    pass
+
+
+def random_move(state, m, n):
+    pass
+
+
 class Wythoff(gym.Env):
     """Wythoff's game.
     
@@ -63,6 +72,19 @@ class Wythoff(gym.Env):
             reward = -1
             done = True
 
+        elif (self.x + dx) < 0:
+            self.x += dx
+            self.y += dy
+
+            reward = -1
+            done = True
+        elif (self.y + dy) < 0:
+            self.x += dx
+            self.y += dy
+
+            reward = -1
+            done = True
+
         # Winning move?
         elif (self.x + dx == 0) and (self.y + dy == 0):
             self.x += dx
@@ -78,41 +100,16 @@ class Wythoff(gym.Env):
             reward = 0
             done = False
 
-        # ------------------------------------
-        # If the game ended the opponent 
-        # can't play...
-        if done:
-            state = (self.x, self.y)
-            return state, reward, done, info
-
-        # ------------------------------------
-        # The opponent plays... perfectly.
-        opt_dx, opt_dy = self.optimal_move()
-
-        if ((self.x + opt_dx) == 0) and ((self.y + opt_dy) == 0):
-            reward = -1
-            done = True
-
-            self.x += opt_dx
-            self.y += opt_dy
-        else:
-            reward = 0
-            done = False
-
-            self.x += opt_dx
-            self.y += opt_dy
-
+        # -
         state = (self.x, self.y)
-
         return state, reward, done, info
 
-    def optimal_move(self):
-        # every step is followed by an optimal opponent play
-        pass
-
     def reset(self):
-        self.x = int(self.prng.randint(1, self.m + 1))
-        self.y = int(self.prng.randint(1, self.n + 1))
+        self.x = int(self.prng.randint(1, self.m))
+        self.y = int(self.prng.randint(1, self.n))
+
+        state = (self.x, self.y)
+        return state
 
     def render(self, mode='human', close=False):
         raise NotImplementedError("TODO")
