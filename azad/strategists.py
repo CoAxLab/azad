91c20a4359b463820707c09c2c21302fdade8568
Erik Peterson <Erik.Exists@gmail.com>
2018-05-21 16:53:37 -0700

    Removed cruft from strategists sub

--- azad/strategists.py
+++ azad/strategists.py
@@ -3,44 +3,6 @@ import torch
 import torch.nn as nn
 
 
-def estimate_expected_value(x, dim):
-    """Estimate the expected value (of a Q-value tensor)"""
-
-    return torch.max(x, dim)
-
-
-def estimate_alp_value(x, dim):
-    """Estimate the expected value using the Muyesser method
-    
-    As described in:
-    
-    Muyesser, N.A., Dunovan, K. & Verstynen, T., 2018. Learning model-based 
-    strategies in simple environments with hierarchical q-networks. , pp.1–29. A
-    vailable at: http://arxiv.org/abs/1801.06689.
-    """
-
-    x = estimate_expected_value(x, dim)
-    x = (x + 1) / 2
-    return x
-
-
-def estimate_hot_cold(x, zeta):
-    """Estimate the hot/coldness of elements in x
-
-    Hot positions are greater than zeta. Cold are lower.
-    """
-
-    if zeta < 0:
-        raise ValueError("zeta must be >= 0.")
-
-    cold = torch.zeros_likes(x)
-    hot = torch.ones_like(x)
-    x = torch.where(x < zeta, x, cold)
-    x = torch.where(x >= zeta, x, hot)
-
-    return x
-
-
 class HotCold(nn.Module):
     """Layers for a Hot-Cold strategy
     
