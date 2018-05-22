c65eb45dac431f34404b6a899b8d261f17b075c1
Erik Peterson <Erik.Exists@gmail.com>
2018-05-22 15:38:21 -0700

    Update EV fns, mine and alps

--- azad/exp/wythoff.py
+++ azad/exp/wythoff.py
@@ -50,7 +50,7 @@ def plot_wythoff_board(board, plot=False, path=None):
 
 def plot_wythoff_expected_values(m, n, model, plot=False, path=None):
     """Plot EVs"""
-    values = wythoff_expected_values(m, n, model)
+    values = expected_value(m, n, model)
 
     # !
     fig = plt.figure()
@@ -66,7 +66,7 @@ def plot_wythoff_expected_values(m, n, model, plot=False, path=None):
         plt.close()
 
 
-def wythoff_expected_values(m, n, model):
+def expected_value(m, n, model):
     """Estimate the expected value of each board position"""
     # Build the matrix a values for each (i, j) board
     values = np.zeros((m, n))
@@ -81,8 +81,32 @@ def wythoff_expected_values(m, n, model):
     return values
 
 
-def estimate_alp_value(x, dim):
-    """Estimate the expected value using the Muyesser method
+def estimate_cold(m, n, model, threshold=0.25):
+    """Estimate cold positions, enforcing symmetry on the diagonal"""
+    values = expected_value(m, n, model)
+    cold = np.zeros_like(values)
+
+    # Cold
+    mask = values < threshold
+    cold[mask] = -1.0
+    cold[mask.transpose()] = -1.0
+
+    return cold
+
+
+def estimate_hot(m, n, model, threshold=0.75):
+    """Estimate hot positions"""
+    values = expected_value(m, n, model)
+    hot = np.zeros_like(values)
+
+    mask = values > threshold
+    hot[mask] = 1.0
+
+    return hot
+
+
+def expected_alp_value(m, n, model):
+    """Estimate the expected value using the 'Muyesser' method
     
     As described in:
     
@@ -91,26 +115,35 @@ def estimate_alp_value(x, dim):
     vailable at: http://arxiv.org/abs/1801.06689.
     """
 
-    values = wythoff_expected_values(m, n, model)
+    values = expected_value(m, n, model)
     values = (values + 1.0) / 2.0
 
     return values
 
 
-def wythoff_hot_cold(m, n, model, threshold=0.25):
-    """Estimate hot/cold positions"""
-    values = wythoff_expected_values(m, n, model)
-    hotcold = np.zeros_like(values)
+def estimate_alp_cold(m, n, model, threshold=0.25, default_value=0.5):
+    """Estimate cold positions, enforcing symmetry on the diagonal"""
+    values = expected_alp_value(m, n, model)
+    cold = np.ones_like(values) * default_value
 
-    hotcold[values > (1 - threshold)] = 1.0
-    hotcold[values < threshold] = -1.0
+    # Cold
+    mask = values < threshold
+    cold[mask] = values[mask]
+    cold[mask.transpose()] = values[mask]
 
-    return hotcold
+    return cold
 
 
-def hot_cold_coordinates(hotcold):
-    xs, ys = hotcold.nonzero()
-    return list(zip(xs, ys))
+def estimate_alp_hot(m, n, model, threshold=0.75, default_value=0.5):
+    """Estimate hot positions"""
+
+    values = expected_alp_value(m, n, model)
+    hot = np.ones_like(values) * default_value
+
+    mask = values > threshold
+    hot[mask] = values[mask]
+
+    return hot
 
 
 def wythoff_1(path,
@@ -132,8 +165,8 @@ def wythoff_1(path,
 
     # -------------------------------------------
     # Log setup    
-    #
-    # csv for archiving select data
+
+    # Create a csv for archiving select data
     f = open(os.path.join(path, "data.csv"), "w")
     csv_writer = csv.writer(f)
     csv_writer.writerow(["trial", "steps", "action_index", "Q", "x", "y"])
@@ -275,6 +308,7 @@ def wythoff_2(path,
               epsilon=0.1,
               gamma=0.8,
               learning_rate=0.1,
+              strategy_name='hot_cold',
               wythoff_name='Wythoff3x3',
               seed=None):
     """A reimplementation of 
@@ -283,4 +317,145 @@ def wythoff_2(path,
      strategies in simple environments with hierarchical q-networks. 
      pp.1–29. Available at: http://arxiv.org/abs/1801.06689.
      """
-    pass
+    # -------------------------------------------
+    # Create path
+    try:
+        os.makedirs(path)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
+
+    # -------------------------------------------
+    # Log setup    
+
+    # Create a csv for archiving select data
+    f = open(os.path.join(path, "data.csv"), "w")
+    csv_writer = csv.writer(f)
+    csv_writer.writerow(["trial", "steps", "action_index", "Q", "x", "y"])
+
+    # Tensorboard log
+    writer = SummaryWriter(log_dir=path)
+
+    # -------------------------------------------
+    # The world is a pebble on a board
+    env = gym.make('{}-v0'.format(wythoff_name))
+    env = wrappers.Monitor(
+        env, './tmp/{}-v0-1'.format(wythoff_name), force=True)
+
+    # -------------------------------------------
+    # Seeding...
+    env.seed(seed)
+    np.random.seed(seed)
+
+    # -------------------------------------------
+    # Valid moves (in this simplified instantiation)
+    possible_actions = [(-1, 0), (0, -1), (-1, -1)]
+
+    # -------------------------------------------
+    # Build a Q agent, its memory, and its optimizer
+
+    # How big is the board?
+    x, y, board = env.reset()
+    env.close()
+    m, n = board.shape
+
+    # Create a model of the right size
+    model = OneLinQN(m * n, len(possible_actions))
+    optimizer = optim.SGD(model.parameters(), lr=learning_rate)
+
+    # -------------------------------------------
+    # Run some trials
+    for trial in range(num_trials):
+        x, y, board = env.reset()
+        board = Tensor(board.reshape(m * n))
+
+        steps = 0
+        while True:
+            # -------------------------------------------
+            # Look at the world and approximate its value.
+            Qs = model(board).squeeze()
+
+            # Make a decision.
+            action_index = epsilon_greedy(Qs, epsilon)
+            action = possible_actions[int(action_index)]
+
+            Q = Qs[int(action_index)]
+
+            (x, y, next_board), reward, done, _ = env.step(action)
+            next_board = Tensor([next_board.reshape(m * n)])
+
+            # Update move counter
+            steps += 1
+
+            # ---
+            # Learn w/ SGD
+            max_Q = model(next_board).detach().max()
+            next_Q = reward + (gamma * max_Q)
+            loss = F.smooth_l1_loss(Q, next_Q)
+
+            optimizer.zero_grad()
+            loss.backward(retain_graph=True)  # retain is needed for opp. WHY?
+            optimizer.step()
+
+            # Shuffle state notation
+            board = next_board
+
+            # -------------------------------------------
+            # Log results
+            csv_writer.writerow(
+                [trial, steps, int(action_index), float(Q), x, y])
+
+            writer.add_scalar(os.path.join(path, 'Q'), Q, trial)
+            writer.add_scalar(os.path.join(path, 'reward'), reward, trial)
+
+            # -------------------------------------------
+            # If the game is over, stop
+            if done:
+                break
+
+            # -------------------------------------------
+            # Otherwise the opponent moves
+            action_index = np.random.randint(0, len(possible_actions))
+            action = possible_actions[action_index]
+
+            Q = Qs[int(action_index)].squeeze()
+
+            (x, y, next_board), reward, done, info = env.step(action)
+            next_board = Tensor([next_board.reshape(m * n)])
+
+            # Flip signs so opp victories are punishments
+            if reward > 0:
+                reward *= -1
+
+            # -
+            writer.add_scalar(os.path.join(path, 'opp_Q'), Q, trial)
+            writer.add_scalar(os.path.join(path, 'opp_reward'), reward, trial)
+
+            # ---
+            # Learn from your opponent
+            max_Q = model(next_board).detach().max()
+            next_Q = reward + (gamma * max_Q)
+            loss = F.smooth_l1_loss(Q, next_Q)
+
+            # optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            # Plot?
+            if (trial % 10) == 0:
+                plot_wythoff_expected_values(m, n, model, path=path)
+
+                writer.add_image(
+                    'expected_value',
+                    skimage.io.imread(
+                        os.path.join(path, 'wythoff_expected_values.png')))
+
+            if done:
+                break
+
+        # save min loss for this trial
+        writer.add_scalar(os.path.join(path, 'error'), loss.data[0], trial)
+
+    # Cleanup and end
+    writer.close()
+    f.close()
