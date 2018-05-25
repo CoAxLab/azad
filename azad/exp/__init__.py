1bb4195a5b3f134980fcad215c22cdf51a3c9794
Erik Peterson <Erik.Exists@gmail.com>
2018-05-24 18:47:56 -0700

    wythoff_strategist now exportable/playable

--- azad/exp/__init__.py
+++ azad/exp/__init__.py
@@ -2,3 +2,4 @@ from azad.exp import run_azad
 from azad.exp.cart import cart_stumbler
 from azad.exp.bandit import bandit_stumbler
 from azad.exp.wythoff import wythoff_stumbler
+from azad.exp.wythoff import wythoff_strategist
