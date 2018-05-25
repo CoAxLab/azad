3af6d3b0a25ffdd197bd9ff75c00c6c5b549f884
Erik Peterson <Erik.Exists@gmail.com>
2018-05-24 18:21:23 -0700

    Added greedy policy

--- azad/policy.py
+++ azad/policy.py
@@ -1,7 +1,18 @@
 import torch
 
 
+def greedy(x, epsilon):
+    """Pick the biggest"""
+    action = torch.argmax(x).unsqueeze(0)
+
+    # Ensure type consistency... 
+    action = action.type(torch.FloatTensor)
+
+    return action
+
+
 def epsilon_greedy(x, epsilon):
+    """Pick the biggest, with probability epsilon"""
     if torch.rand(1) > epsilon:
         action = torch.argmax(x).unsqueeze(0)
     else:
