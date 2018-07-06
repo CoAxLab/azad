4af45e1d1a1ada67a675ec9dd7c3c0ebf61c6a4b
Erik Peterson <Erik.Exists@gmail.com>
2018-07-06 15:47:15 -0700

    WIP: strategist
    seems to be learning for individual trials...
    
    but board plots don't show it.

--- azad/exp/wythoff.py
+++ azad/exp/wythoff.py
@@ -45,8 +45,15 @@ from azad.util.wythoff import pad_board
 from azad.util.wythoff import flatten_board
 from azad.util.wythoff import convert_ijv
 from azad.util.wythoff import balance_ijv
+from azad.util.wythoff import evaluate_models
+from azad.util.wythoff import estimate_cold
+from azad.util.wythoff import estimate_hot
+from azad.util.wythoff import estimate_hot_cold
+from azad.util.wythoff import estimate_alp_hot_cold
+from azad.util.wythoff import estimate_strategic_value
 from azad.util.wythoff import create_env
 from azad.util.wythoff import create_moves
+from azad.util.wythoff import create_bias_board
 from azad.util.wythoff import create_all_possible_moves
 from azad.util.wythoff import create_cold_board
 from azad.util.wythoff import _np_epsilon_greedy
@@ -188,7 +195,6 @@ def wythoff_agent(path,
             model[grad_board][grad_i] = Q + (learning_rate * loss)
 
             # ----------------------------------------------------------------
-            # if tensorboard and (int(trial) % 50) == 0:
             if debug:
                 print(">>> Reward {}; Loss(Q {}, next_Q {}) -> {}".format(
                     reward, Q, next_Q, loss))
@@ -233,6 +239,7 @@ def wythoff_stumbler(path,
                      learning_rate=0.1,
                      game='Wythoff3x3',
                      model=None,
+                     env=None,
                      bias_board=None,
                      tensorboard=False,
                      debug=False,
@@ -253,7 +260,9 @@ def wythoff_stumbler(path,
         writer = SummaryWriter(log_dir=path)
 
     # Crate env
-    env = create_env(game)
+    if env is None:
+        env = create_env(game)
+
     env.seed(seed)
     np.random.seed(seed)
 
@@ -284,7 +293,7 @@ def wythoff_stumbler(path,
 
         if debug:
             print("---------------------------------------")
-            print(">>> NEW GAME ({}).".format(trial))
+            print(">>> STUMBLER ({}).".format(trial))
             print(">>> Initial position ({}, {})".format(x, y))
             print(">>> Initial moves {}".format(moves))
 
@@ -369,7 +378,7 @@ def wythoff_stumbler(path,
                 if done and (reward < 0):
                     print("*** OPPONENT WIN ***")
 
-            if tensorboard and (int(trial) % 50) == 0:
+            if tensorboard:  # and (int(trial) % 50) == 0:
                 writer.add_graph(model, (board, ))
 
                 writer.add_scalar(os.path.join(path, 'reward'), reward, trial)
@@ -380,16 +389,24 @@ def wythoff_stumbler(path,
                 # Optimal ref:
                 plot_cold_board(m, n, path=path, name='cold_board.png')
                 writer.add_image(
-                    'cold_positions',
+                    'True cold positions',
                     skimage.io.imread(os.path.join(path, 'cold_board.png')))
 
                 # EV:
                 plot_wythoff_expected_values(
                     m, n, model, path=path, name='wythoff_expected_values.png')
                 writer.add_image(
-                    'expected_value',
+                    'Max value',
                     skimage.io.imread(
                         os.path.join(path, 'wythoff_expected_values.png')))
+
+                est_hc_board = estimate_hot_cold(
+                    m, n, model, hot_threshold=0.5, cold_threshold=0.0)
+                plot_wythoff_board(
+                    est_hc_board, path=path, name='est_hc_board.png')
+                writer.add_image(
+                    'Estimated hot and cold positions',
+                    skimage.io.imread(os.path.join(path, 'est_hc_board.png')))
 
     # ------------------------------------------------------------------------
     # The end
@@ -417,53 +434,65 @@ def wythoff_stumbler(path,
 def wythoff_strategist(
         path,
         num_trials=1000,
-        num_stumbles=100,
-        epsilon=0.1,
+        num_stumbles=1,
+        num_evals=1,
+        epsilon=0.2,
         gamma=0.8,
-        delta=0.1,
-        learning_rate=0.1,
+        strategist_learning_rate=0.01,
+        stumbler_learning_rate=0.1,
         stumbler_game='Wythoff15x15',
         strategist_game='Wythoff50x50',
         num_hidden1=15,
         #    num_hidden1=25,
         #    num_hidden2=100,
-        log=False,
+        tensorboard=False,
+        debug=False,
         seed=None):
 
-    # -------------------------------------------
+    # ------------------------------------------------------------------------
     # Setup
-    env = create_env(strategist_game)
-    possible_actions = [(-1, 0), (0, -1), (-1, -1)]
 
-    # Working mem size
-    batch_size = 12
+    # Create path
+    try:
+        os.makedirs(path)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
+
+    if tensorboard:
+        writer = SummaryWriter(log_dir=path)
+
+    # Create env and find all moves in it
+    env = create_env(strategist_game)
+    m, n, board, _ = peek(env)
+    all_possible_moves = create_all_possible_moves(m, n)
+
+    # Peek at stumbler env
+    o, p, _, _ = peek(create_env(stumbler_game))
 
     # Control randomness
     env.seed(seed)
     np.random.seed(seed)
 
-    if log:
+    # Working mem size
+    batch_size = 12
+
+    if tensorboard:
         writer = SummaryWriter(log_dir=path)
 
-    # -------------------------------------------
+    # ------------------------------------------------------------------------
     # Build a Strategist, its memory, and its optimizer
-
-    # Board sizes....
-    m, n, board = peek(env)
-    o, p, _ = peek(create_env(stumbler_game))
-
-    # Def the strategist model, opt, and mem
-    # model = HotCold3(2, num_hidden1=num_hidden1, num_hidden2=num_hidden2)
     model = HotCold2(2, num_hidden1=num_hidden1)
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    optimizer = optim.Adam(model.parameters(), lr=strategist_learning_rate)
     memory = ReplayMemory(10000)
 
-    # -------------------------------------------
-    wins = []
+    # ------------------------------------------------------------------------
     stumbler_model = None
+    stumbler_env = None
     bias_board = None
+    influence = 0.0
     for trial in range(num_trials):
-
+        # --------------------------------------------------------------------
         stumbler_model, stumbler_env = wythoff_stumbler(
             path,
             num_trials=num_stumbles,
@@ -471,100 +500,130 @@ def wythoff_strategist(
             gamma=gamma,
             game=stumbler_game,
             model=stumbler_model,
-            bias_board=bias_board,
-            learning_rate=learning_rate)
+            env=stumbler_env,
+            bias_board=None,
+            learning_rate=stumbler_learning_rate,
+            tensorboard=False,
+            debug=debug,
+            seed=seed)
+
+        if debug:
+            print("---------------------------------------")
+            print(">>> STRATEGIST ({}).".format(trial))
 
         # Extract strategic data from the stumber,
         # project it and remember that
         strategic_default_value = 0.0
-        strategic_value = -1 * create_cold_board(o, p)
-
-        # strategic_default_value = 0.5
-        # strategic_value = estimate_alp_hot_cold(
-        #     o, p, stumbler_model, conf=0.05, default=strategic_default_value)
+        strategic_value = estimate_cold(o, p, stumbler_model, threshold=0.0)
         strategic_value = pad_board(m, n, strategic_value,
                                     strategic_default_value)
+
+        # strategic_value = estimate_hot_cold(
+        #     o, p, stumbler_model, hot_threshold=0.5, cold_threshold=0.0)
 
         # ...Into tuples
         s_data = convert_ijv(strategic_value)
         s_data = balance_ijv(s_data, strategic_default_value)
 
-        for d in s_data:
-            memory.push(*d)
+        if s_data is not None:
+            for d in s_data:
+                memory.push(*d)
 
-        # Sample data....
-        coords = []
-        values = []
-        samples = memory.sample(batch_size)
+        loss = 0.0
+        if len(memory) > batch_size:
+            # Sample data....
+            coords = []
+            values = []
+            samples = memory.sample(batch_size)
 
-        for c, v in samples:
-            coords.append(c)
-            values.append(v)
+            for c, v in samples:
+                coords.append(c)
+                values.append(v)
 
-        coords = torch.tensor(
-            np.vstack(coords), requires_grad=True, dtype=torch.float)
-        values = torch.tensor(values, requires_grad=False, dtype=torch.float)
+            coords = torch.tensor(
+                np.vstack(coords), requires_grad=True, dtype=torch.float)
+            values = torch.tensor(
+                values, requires_grad=False, dtype=torch.float)
 
-        # Making some preditions,
-        predicted_values = model(coords).squeeze()
+            # Making some preditions,
+            predicted_values = model(coords).squeeze()
 
-        # and find their loss.
-        loss = F.mse_loss(predicted_values, values)
+            # and find their loss.
+            loss = F.mse_loss(predicted_values, values)
 
-        # Walk down the hill of righteousness!
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+            # Walk down the hill of righteousness!
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
+            if debug:
+                print(">>> Coords: {}".format(coords))
+                print(">>> Values: {}".format(values))
+                print(">>> Predicted values: {}".format(values))
+                print(">>> Loss {}".format(loss))
+
+                print(">>> Last win {}".format(win))
+                print(">>> Influence {}".format(influence))
+
+        # --------------------------------------------------------------------
         # Use the trained strategist to generate a bias_board,
-        bias_board = delta * estimate_strategic_value(m, n, model)
-        # bias_board = delta * strategic_value
+        bias_board = estimate_strategic_value(m, n, model)
 
-        # Compare strategist and stumbler. Count strategist wins.
-        win = evaluate_models(stumbler_model, model, stumbler_env, env)
+        # Est performance. Count strategist wins.
+        win = evaluate_models(
+            stumbler_model,
+            model,
+            stumbler_env,
+            env,
+            num_eval=num_evals,
+            debug=debug)
 
-        if log:
+        # Update the influence and then the bias_board
+        if win > 0.5:
+            influence += strategist_learning_rate
+        else:
+            influence -= strategist_learning_rate
+
+        influence = np.clip(influence, 0, 1)
+        bias_board *= influence
+
+        # --------------------------------------------------------------------
+        if tensorboard and (int(trial) % 50) == 0:
+            # Timecourse
             writer.add_scalar(
-                os.path.join(path, 'stategist_error'), loss.data[0], trial)
+                os.path.join(path, 'stategist_error'), loss, trial)
+            writer.add_scalar(os.path.join(path, 'Stategist_wins'), win, trial)
+            writer.add_scalar(
+                os.path.join(path, 'Stategist_influence'), influence, trial)
 
-            writer.add_scalar(os.path.join(path, 'stategist_wins'), win, trial)
-
+            # Boards
             plot_wythoff_expected_values(
-                o, p, stumbler_model, vmin=-3, vmax=3, path=path)
+                o, p, stumbler_model, vmin=-2, vmax=2, path=path)
             writer.add_image(
-                'expected_value',
+                'Stumbler_expected_value',
                 skimage.io.imread(
                     os.path.join(path, 'wythoff_expected_values.png')))
 
+            est_hc_board = estimate_hot_cold(
+                o, p, stumbler_model, hot_threshold=0.5, cold_threshold=0.0)
             plot_wythoff_board(
-                strategic_value, path=path, name='strategy_board.png')
+                est_hc_board, path=path, name='est_hc_board.png')
             writer.add_image(
-                'strategic_value',
-                skimage.io.imread(os.path.join(path, 'strategy_board.png')))
+                'Stumbler_hot_cold',
+                skimage.io.imread(os.path.join(path, 'est_hc_board.png')))
 
-            plot_wythoff_board(bias_board, path=path, name='bias_board.png')
+            plot_wythoff_board(
+                bias_board, vmin=0, vmax=1, path=path, name='bias_board.png')
             writer.add_image(
-                'strategist_model',
+                'Strategist learning',
                 skimage.io.imread(os.path.join(path, 'bias_board.png')))
 
-            plot_q_action_values(
-                o,
-                p,
-                len(possible_actions),
-                stumbler_model,
-                possible_actions=possible_actions,
-                path=path,
-                name='q_action_values.png')
-
-            writer.add_image(
-                'q_action_values',
-                skimage.io.imread(os.path.join(path, 'q_action_values.png')))
-
+    # ------------------------------------------------------------------------
     # The end
-    if log:
+    if tensorboard:
         writer.close()
 
-    return model, stumbler_model, env, stumbler_env, wins
+    return (model, env, influence)
 
 
 def wythoff_optimal(
@@ -665,7 +724,7 @@ def wythoff_optimal(
         # Use the trained strategist to generate a bias_board,
         bias_board = estimate_strategic_value(m, n, model)
 
-        if log:
+        if tensorboard:
             writer.add_scalar(os.path.join(path, 'error'), loss.data[0], trial)
 
             plot_wythoff_board(
@@ -688,4 +747,4 @@ def wythoff_optimal(
     if log:
         writer.close()
 
-    return model, env
+    return model, env,
