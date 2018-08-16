698c1438055ed144227cff61e6a4d72d411f5df8
Erik Peterson <Erik.Exists@gmail.com>
2018-08-16 13:43:10 -0700

    Fixed Nim importing

--- azad/local_gym/nim.py
+++ azad/local_gym/nim.py
@@ -21,7 +21,7 @@ class NimEnv(WythoffEnv):
     Note: subclass to use."""
 
     def __init__(self, m, n, seed=None):
-        super(NimEnv).__init__(m, n, seed)
+        super().__init__(m, n, seed)
 
     def _create_moves(self):
         self.moves = create_moves(self.x, self.y)
@@ -31,32 +31,32 @@ class Nim3x3(NimEnv):
     """A 3 by 3 Nim game"""
 
     def __init__(self):
-        WythoffEnv.__init__(self, m=3, n=3)
+        NimEnv.__init__(self, m=3, n=3)
 
 
 class Nim5x5(NimEnv):
     """A 5 by 5 Nim game"""
 
     def __init__(self):
-        WythoffEnv.__init__(self, m=5, n=5)
+        NimEnv.__init__(self, m=5, n=5)
 
 
 class Nim10x10(NimEnv):
     """A 10 by 10 Nim game"""
 
     def __init__(self):
-        WythoffEnv.__init__(self, m=10, n=10)
+        NimEnv.__init__(self, m=10, n=10)
 
 
 class Nim15x15(NimEnv):
     """A 15 by 15 Nim game"""
 
     def __init__(self):
-        WythoffEnv.__init__(self, m=15, n=15)
+        NimEnv.__init__(self, m=15, n=15)
 
 
 class Nim50x50(NimEnv):
     """A 50 by 50 Nim game"""
 
     def __init__(self):
-        WythoffEnv.__init__(self, m=50, n=50)
+        NimEnv.__init__(self, m=50, n=50)
