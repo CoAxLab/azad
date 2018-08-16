e2b76d5ab064f78f1eff5dbc5b6c6f3a0d4f8c13
Erik Peterson <Erik.Exists@gmail.com>
2018-08-16 10:03:01 -0700

    Added nim to exportable games

--- azad/local_gym/__init__.py
+++ azad/local_gym/__init__.py
@@ -15,21 +15,32 @@ from .wythoff import Wythoff10x10
 from .wythoff import Wythoff15x15
 from .wythoff import Wythoff50x50
 
-environments = [['BanditTenArmedRandomFixed', 'v0', 1],
-                ['BanditTenArmedRandomRandom', 'v0', 1],
-                ['BanditTenArmedGaussian', 'v0', 1],
-                ['BanditTenArmedUniformDistributedReward', 'v0', 1],
-                ['BanditTwoArmedDeterministicFixed', 'v0', 1],
-                ['BanditTwoArmedHighHighFixed', 'v0', 1],
-                ['BanditTwoArmedHighLowFixed', 'v0', 1],
-                ['BanditTwoArmedLowLowFixed', 'v0', 1],
-                ['Wythoff3x3', 'v0', 1000], ['Wythoff5x5', 'v0', 1000],
-                ['Wythoff10x10', 'v0', 1000], ['Wythoff15x15', 'v0', 1000],
-                ['Wythoff50x50', 'v0', 1000]]
+from .nim import Nim3x3
+from .nim import Nim5x5
+from .nim import Nim10x10
+from .nim import Nim15x15
+from .nim import Nim50x50
+
+environments = [['BanditTenArmedRandomFixed', 'v0',
+                 1], ['BanditTenArmedRandomRandom', 'v0',
+                      1], ['BanditTenArmedGaussian', 'v0', 1],
+                ['BanditTenArmedUniformDistributedReward', 'v0',
+                 1], ['BanditTwoArmedDeterministicFixed', 'v0',
+                      1], ['BanditTwoArmedHighHighFixed', 'v0',
+                           1], ['BanditTwoArmedHighLowFixed', 'v0',
+                                1], ['BanditTwoArmedLowLowFixed', 'v0',
+                                     1], ['Wythoff3x3', 'v0', 1000],
+                ['Wythoff5x5', 'v0', 1000], ['Wythoff10x10', 'v0', 1000], [
+                    'Wythoff15x15', 'v0', 1000
+                ], ['Wythoff50x50', 'v0', 1000], ['Nim3x3', 'v0', 1000], [
+                    'Nim5x5', 'v0', 1000
+                ], ['Nim10x10', 'v0', 1000], ['Nim15x15', 'v0',
+                                              1000], ['Nim50x50', 'v0', 1000]]
 
 for environment in environments:
     register(
         id='{}-{}'.format(environment[0], environment[1]),
         entry_point='azad.local_gym:{}'.format(environment[0]),
         timestep_limit=environment[2],
-        nondeterministic=True, )
+        nondeterministic=True,
+    )
