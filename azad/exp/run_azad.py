706353e8ec330620b0c31dad7b2cb81cc9f4d434
Erik Peterson <Erik.Exists@gmail.com>
2018-08-17 14:11:46 -0700

    Cleanup

--- azad/exp/run_azad.py
+++ azad/exp/run_azad.py
@@ -4,20 +4,8 @@ import fire
 
 from azad import exp as a_exps
 
-# def exp_build():
-#     raise NotImplementedError("TODO.")
-
-# def exp_list(details=False):
-#     """List all registered experiments"""
-#     # Loop over all run in this submodule
-#     # if the fn name is exp_INT print its name
-#     # and print its docstring
-#     raise NotImplementedError("TODO.")
-
 if __name__ == "__main__":
-    # Auto build a CL API from all azad.exps,
-    # but first add a couple local things.
-    # cl = {"exp_list": exp_list, "exp_build": exp_build}
+    # Auto build a CL API from all azad.exps.
     cl = {}
 
     # Get all the attrs on azad.exp
