3d060701d27c6d8e6fb0db68ae1115e5c2de3600
Erik Peterson <Erik.Exists@gmail.com>
2018-08-21 13:16:19 -0700

    Moved common notebook fns to importable

---
+++ notebooks/notebook_helpers.py
@@ -0,0 +1,49 @@
+from collections import defaultdict
+from csv import DictReader
+import numpy as np
+
+
+def load_params(file):
+    """Custom param loading fn"""
+    table = defaultdict(list)
+    with open(file, 'r') as f:
+        reader = DictReader(f)
+        for row in reader:
+            for col, dat in row.items():
+                table[col].append(float(dat))
+    return table
+
+
+def load_monitored(file):
+    """Load monitored data."""
+
+    table = defaultdict(list)
+    file_name = file.split('/')[-1].split('.')[0]
+
+    with open(file, 'r') as f:
+        reader = DictReader(f)
+        for row in reader:
+            table["file"].append(file_name)
+
+            for col, dat in row.items():
+                table[col].append(float(dat))
+
+    return table
+
+
+def join_monitored(files, sort_key='episode'):
+    """Join several monitored datasets, by column."""
+
+    joined = load_monitored(files[0])
+
+    for fi in files[1:]:
+        for k, v in load_monitored(fi).items():
+            joined[k].extend(v)
+
+    if sort_key is not None:
+        idx = np.argsort(joined[sort_key]).tolist()
+
+        for k, v in joined.items():
+            joined[k] = [v[i] for i in idx]
+
+    return joined
