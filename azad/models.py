a350c30ac7b32d7c76c55b61d464bff072201ba7
Erik Peterson <Erik.Exists@gmail.com>
2019-12-11 15:35:49 -0800

    Fix Conv dimensions in the DQN

--- azad/models.py
+++ azad/models.py
@@ -50,10 +50,10 @@ class DQN(nn.Module):
             to action in game.
         """
         super(DQN, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, 32, kernel_size=8, stride=4)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1)
         self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
-        self.fc4 = nn.Linear(7 * 7 * 64, 512)
+        self.fc4 = nn.Linear(1024, 512)
         self.fc5 = nn.Linear(512, num_actions)
 
     def forward(self, x):
