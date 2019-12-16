2523ae4e32cca3773c32004e659ee235037117b3
Erik Peterson <Erik.Exists@gmail.com>
2019-12-16 11:50:44 -0800

    Update imports

--- azad/exp/__init__.py
+++ azad/exp/__init__.py
@@ -6,5 +6,6 @@ from azad.exp.wythoff import wythoff_strategist
 from azad.exp.wythoff import wythoff_stumbler_strategist
 from azad.exp.wythoff import wythoff_oracular_strategy
 from azad.exp.wythoff import evaluate_wythoff
+from azad.exp.alternatives import wythoff_dqn1
 from azad.exp.search import create_grid
 from azad.exp.search import create_random
