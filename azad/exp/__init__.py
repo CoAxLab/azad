5129433e7b355e5756e3818e1d4de7d44192eb87
Erik Peterson <Erik.Exists@gmail.com>
2019-12-19 13:02:12 -0800

    Update API to show all dqn exps

--- azad/exp/__init__.py
+++ azad/exp/__init__.py
@@ -7,5 +7,7 @@ from azad.exp.wythoff import wythoff_stumbler_strategist
 from azad.exp.wythoff import wythoff_oracular_strategy
 from azad.exp.wythoff import evaluate_wythoff
 from azad.exp.alternatives import wythoff_dqn1
+from azad.exp.alternatives import wythoff_dqn2
+from azad.exp.alternatives import wythoff_dqn3
 from azad.exp.search import create_grid
 from azad.exp.search import create_random
