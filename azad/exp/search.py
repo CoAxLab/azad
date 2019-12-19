f7011b6256534e0041034ed978b7ef35b35bc058
Erik Peterson <Erik.Exists@gmail.com>
2019-12-19 12:34:04 -0800

    Add gpu option to grid_search

--- azad/exp/search.py
+++ azad/exp/search.py
@@ -1,10 +1,16 @@
 import numpy as np
 from itertools import product
+from itertools import cycle
 
 
-def create_grid(name, fmt=None, **kwargs):
+def create_grid(name, fmt=None, num_gpu=0, **kwargs):
     """Create a .csv for doing a grid param search."""
+    # Sanity
+    num_gpu = int(num_gpu)
+    if num_gpu < 0:
+        raise ValueError("num_gpu must be positive")
 
+    # Create the grid
     keys = sorted(list(kwargs.keys()))
     values = []
     for k in keys:
@@ -12,15 +18,32 @@ def create_grid(name, fmt=None, **kwargs):
         v = np.linspace(start, stop, n)
         values.append(v)
 
+    # then rearrange it into a nice csv/table
     table = product(*values)
     i_table = []
-    for i, t in enumerate(table):
-        i_table.append((i, *t))
-    table = np.vstack(i_table)
 
-    head = "row_code," + ",".join(keys)
-    if fmt is None:
-        fmt = '%i,' + '%.6f,' * (len(keys) - 1) + '%.6f'
+    # The table depends on GPU use, so...
+    # No GPU
+    if num_gpu == 0:
+        for i, t in enumerate(table):
+            i_table.append((i, *t))
+
+        head = "row_code," + ",".join(keys)
+        if fmt is None:
+            fmt = '%i,' + '%.6f,' * (len(keys) - 1) + '%.6f'
+
+    # Use GPU(s). Dividing the work equally between 'em.
+    else:
+        device_count = cycle(range(num_gpu))
+        for i, t in enumerate(table):
+            i_table.append((i, next(device_count), *t))
+
+        head = "row_code,device_code," + ",".join(keys)
+        if fmt is None:
+            fmt = '%i,%i,' + '%.6f,' * (len(keys) - 1) + '%.6f'
+
+    # Form final table and save it.
+    table = np.vstack(i_table)
     np.savetxt(name, table, delimiter=",", header=head, fmt=fmt, comments="")
 
 
