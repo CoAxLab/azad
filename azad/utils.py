dcc689df1042eff7adc719614625c21d0394a8af
Erik Peterson <Erik.Exists@gmail.com>
2019-12-19 14:45:25 -0800

    Add utils sub

---
+++ azad/utils.py
@@ -0,0 +1,16 @@
+import cloudpickle
+import numpy as np
+import torch
+from typing import List, Tuple
+from scipy.linalg import eigh
+
+
+def save_checkpoint(state, filename='checkpoint.pkl'):
+    data = cloudpickle.dumps(state)
+    with open(filename, 'wb') as fi:
+        fi.write(data)
+
+
+def load_checkpoint(filename='checkpoint.pkl'):
+    with open(filename, 'rb') as fi:
+        return cloudpickle.load(fi)
