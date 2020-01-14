16c425aec64fd61a76c1d6748a1074449442d3f7
Erik Peterson <Erik.Exists@gmail.com>
2020-01-14 15:25:34 -0800

    wythoff_az - Add DNN to saved result

--- azad/exp/alternatives/wythoff_alphazero.py
+++ azad/exp/alternatives/wythoff_alphazero.py
@@ -184,7 +184,7 @@ def wythoff_alphazero(num_episodes=10,
     if monitor:
         save_monitored(save, monitored)
 
-    result = dict(mcts=history, score=score)
+    result = dict(mcts=history, network=network, score=score)
     if save is not None:
         save_checkpoint(result, filename=save + ".pkl")
     else:
