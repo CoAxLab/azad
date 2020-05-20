f70d054588bba879cacc75a6e77aff86b44634eb
Erik Peterson <Erik.Exists@gmail.com>
2020-05-20 09:33:02 -0700

    FIX - export wythoff_dqn2

--- azad/exp/alternatives/__init__.py
+++ azad/exp/alternatives/__init__.py
@@ -3,7 +3,7 @@ from azad.exp.alternatives.wythoff_dqn2 import wythoff_dqn2
 from azad.exp.alternatives.wythoff_dqn3 import wythoff_dqn3
 from azad.exp.alternatives.wythoff_dqn4 import wythoff_dqn4
 from azad.exp.alternatives.optuna_dqn2 import optuna_dqn2
-from azad.exp.alternatives.wythoff_dqn3 import evaluate_dqn2
+from azad.exp.alternatives.wythoff_dqn2 import evaluate_dqn2
 from azad.exp.alternatives.wythoff_dqn3 import evaluate_dqn3
 from azad.exp.alternatives.wythoff_mcts import wythoff_mcts
 from azad.exp.alternatives.wythoff_alphazero import wythoff_alphazero
